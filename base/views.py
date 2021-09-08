36390d5abfcd978b2c77a631ba83d844697f3668
gautamsood15 <gauty22@gmail.com>
2021-09-08 16:27:21 +0530

    importing detailview

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.detail import View
+from django.views.generic.detail import DetailView
 from .models import Task
 
 # Create your views here.
