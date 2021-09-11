26dd3f3f1cfdc154a2e58f871771330b51c8b014
gautamsood15 <gauty22@gmail.com>
2021-09-11 16:52:03 +0530

    removing absolute urls

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,5 @@ from .views import TaskList, TaskDetail
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('/task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 ]
