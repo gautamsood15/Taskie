c2de8857d613c5ea3200ece298d08036465fe2ae
gautamsood15 <gauty22@gmail.com>
2021-09-14 12:27:17 +0530

    adding task create url

--- base/urls.py
+++ base/urls.py
@@ -4,4 +4,5 @@ from .views import TaskList, TaskDetail, TaskCreate
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-create/', TaskCreate.as_view(), name='task-create'),
 ]
