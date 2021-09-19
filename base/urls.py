7dc6fac538a7fc77d32876943655341719fe6f27
gautamsood15 <gauty22@gmail.com>
2021-09-19 15:21:03 +0530

    importing task update to urls

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
