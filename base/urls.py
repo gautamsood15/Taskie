e02e711aa0a54ec6d7af9889e1cce026aebf13b8
gautamsood15 <gauty22@gmail.com>
2021-09-21 15:22:05 +0530

    adding new url paths

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 ]
