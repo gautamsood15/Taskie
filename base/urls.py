90d172234da1d5725d1cdfde1875a8424f42124f
gautamsood15 <gauty22@gmail.com>
2021-09-22 15:26:32 +0530

    adding task update view to url

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
-    path('task-update/<int:pk>/', TaskDetail.as_view(), name='task'),
+    path('task-update/<int:pk>/', TaskUpdate.as_view(), name='task'),
 ]
