079b762ba9824da7bb6d8b052609418d2331382c
gautamsood15 <gauty22@gmail.com>
2021-10-02 12:42:28 +0530

    adding blank to textfield

--- base/models.py
+++ base/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     complete = models.BooleanField()
     created = models.DateTimeField()
