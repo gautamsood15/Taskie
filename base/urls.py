d5768656e13211c04f98dcad23bbc44070be0b6e
gautamsood15 <gauty22@gmail.com>
2021-10-05 09:50:09 +0530

    importing custom login view to urls

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
