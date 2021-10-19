097db95a03a08e10d2462c66ed03ade14ac47d70
gautamsood15 <gauty22@gmail.com>
2021-10-19 09:26:29 +0530

    returning red context

--- base/views.py
+++ base/views.py
@@ -27,6 +27,7 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['color'] = 'red'
+        return context
         
 
 class TaskDetail(LoginRequiredMixin, DetailView):
