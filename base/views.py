0742b4d0e5d5e3425fbb27885dfc9c93c026c1fa
gautamsood15 <gauty22@gmail.com>
2021-10-28 01:00:56 +0530

    creating login template test compelete

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import login
+from django.contrib.auth import login
 
 from .models import Task
 
